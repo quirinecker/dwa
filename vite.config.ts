faadac6e35f3682dc31411399f8e396de54ae00b
quirinecker <quirin.ecker@bajupa.com>
2024-03-05 14:03:53 +0100

    added tailwind and pwa plugin for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue(), VitePWA({
+        registerType: 'autoUpdate',
+        manifest: {
+            name: 'DWA',
+            theme_color: 'black',
+            icons: [
+                { src: '/logo.svg', type: 'image/svg+xml', sizes: '256x256' }
+            ]
+        }
+    })],
+    css: {
+        postcss: {
+            plugins: [tailwind(), autoprefixer()]
+        }
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 })
